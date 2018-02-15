bed6b061ad95faae9654bc2cb20c8e36c4110039
arcticdreamz <tomcrasset@gmail.com>
2018-02-15 01:29:14 +0100

    Added while loop, added exit, added status, added basic command reading but that doesn't work

--- main.c
+++ main.c
@@ -8,114 +8,135 @@
 #include <errno.h>
 
 
-int start_shell(char** arg){
-	// argv[0] = (char *) NULL ; Must be null terminated
-	// 
-	// 
-	pid_t pid;
-	int status;
+int main(){
 
- 	pid = fork();
+    char* arg[2];
 
-    if(pid < 0){
-        int errnum = errno;
 
-        perror("Process creation failed");
-        fprintf(stderr, "Value of errno: %d\n",errno);
-        fprintf(stderr, "Error: %s \n",strerror(errnum));
-        exit(1);
-    }
-    if(pid == 0){ //This is the son
+    bool stop = false;
+    char command[255];
+    strcpy(command,"");
+    int returnvalue;
 
-       	printf("Child : %d \n",pid);
 
-        char* pathstring = getenv("PATH"); //get the $PATH environment variable
-        //printf(" All Paths : %s \n",pathstring);
-        char** paths = malloc(sizeof(char*)); 
+    pid_t pid;
+    int status;
 
-        int i = 0;
-        char* token = strtok(pathstring,":"); //Parse the string for a path delimited by ":"
+    while(!stop){
 
-        while(token != NULL){
-            paths = realloc(paths,(i+1)*sizeof(char*)); //For each new found path, increase the array size
-            paths[i] = token;
+        printf("> ");
+        fflush(stdout);
+
+
+        // getting the input from stdin
+
+        // 1) Exit the shell (using Ctrl+D or exit())
+        if(fgets(command,256,stdin) == NULL || !strcmp(command,"exit\n")){
+            stop = true;
+            break;
+        }
+        // Rerun the loop if we press 'Enter'
+        if(!strcmp(command,"\n"))
+            continue;
             
-            //printf("Paths[%d]  : %s \n",i,paths[i]);
-
-            token = strtok(NULL,":"); //Parse the array for the next path delimited by ":"
-            i++;
-        }
-
-        //printf("arg[0] : %s \n",arg[0]);
-
-        int j = 0;
-        
-
-        while(j < i){
-            char  path[255] = "";
-            strcat(path,paths[j]);
-            strcat(path,"/");
-            strcat(path,arg[0]);
-            //printf("Path %d : %s \n",j,path);
-
-
-            j++;
-        
-            //printf("access of path %d : %d \n",j,access(path,X_OK));
-
-           if(access(path,X_OK) == 0){
-                //printf("Executable pathPath: %s \n",path);
-            	if(execvp(path,arg) == -1){
-                    int errnum = errno;
-                    perror("Instruction failed");
-                    fprintf(stderr, "Value of errno: %d\n",errno);
-                    fprintf(stderr, "Error: %s \n",strerror(errnum));
-                    //continue;
-                }
-                break;
-            }
-
-        }
-
-
-
-    	exit(1);
-    }
-
-    else{//This is the father
-    	printf("Parent : %d \n",pid);
-    	while(true){
-		    wait(&status);
-    		if(WIFEXITED(status))
-    			break;
-    	}
-    }
-
-    return 1;
-}
-
-int main(/*int argc, char* argv[]*/){
-
-	char* args[3];
-    printf(">");
-    
-    //char s[2];
-    //scanf("%s",s);
-	
+        printf("command : %s\n",command);
 
 /**************************************************************************************
 
 
 TU DOIS T'OCCUPER DE CETTE PARTIE ANTOINE : LE PARSING
+        arg[0] = command; ne marche pas, je ne sais pas pq
+        Genre le path est le même, mais il ne veut pas etre exécuté
+
 **************************************************************************************/
+        arg[0] = "ls";
+        printf("arg[0] : %s \n",arg[0]);
+        arg[1] = (char*) NULL;
+
+        //2) The user enters a command
+            // 2).1 : the command is cd
+
+        // 3)the command isn't a built-in command
 
 
-    args[0] = "ls";
-	args[1] = "-l";
-	args[2] = (char*) NULL;
+        pid = fork();
 
-	start_shell(args);
+        if(pid < 0){
+            int errnum = errno;
 
-	return 0;
+            perror("Process creation failed");
+            fprintf(stderr, "Value of errno: %d\n",errno);
+            fprintf(stderr, "Error: %s \n",strerror(errnum));
+            exit(1);
+        }
+        if(pid == 0){ //This is the son
+
+           	printf("Child : %d \n",pid);
+
+            char* pathstring = getenv("PATH"); //get the $PATH environment variable
+            //printf(" All Paths : %s \n",pathstring);
+            char** paths = malloc(sizeof(char*)); 
+
+            int i = 0;
+            char* token = strtok(pathstring,":"); //Parse the string for a path delimited by ":"
+
+            while(token != NULL){
+                paths = realloc(paths,(i+1)*sizeof(char*)); //For each new found path, increase the array size
+                paths[i] = token;
+                
+                //printf("Paths[%d]  : %s \n",i,paths[i]);
+
+                token = strtok(NULL,":"); //Parse the array for the next path delimited by ":"
+                i++;
+            }
+
+            printf("arg[0] : %s \n",arg[0]);
+
+            int j = 0;
+            
+
+            while(j < i){
+                char path[255] = "";
+                strcat(path,paths[j]);
+                strcat(path,"/");
+                strcat(path,arg[0]);
+                //printf("Path %d : %s \n",j,path);
+
+
+                j++;
+            
+                //printf("access of path %d : %d \n",j,access(path,X_OK));
+
+               if(access(path,X_OK) == 0){
+                    printf("Executable path: %s \n",path);
+                	if(execv(path,arg) == -1){
+                        int errnum = errno;
+                        perror("Instruction failed");
+                        fprintf(stderr, "Value of errno: %d\n",errno);
+                        fprintf(stderr, "Error: %s \n",strerror(errnum));
+                        //continue;
+                    }
+                    break;
+                }
+
+            }
+
+
+
+        	exit(1);
+        }
+
+        else{//This is the father
+        	printf("Parent : %d \n",pid);
+    	    wait(&status);
+            returnvalue = WEXITSTATUS(status);
+            printf("%d",returnvalue);
+        	
+        }
+
+
+    }
+
+    return 0;
 
 }
